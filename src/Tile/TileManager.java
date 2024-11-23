448e1af81206de5f47fd3bd1760d134a9008be52
Nguyễn Bạo <robertpamvn@gmail.com>
2024-11-23 19:07:04 +0700

    - clean code; - reactive optimize map algorithm ;

--- src/Tile/TileManager.java
+++ src/Tile/TileManager.java
@@ -10,42 +10,55 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Objects;
 
-public class TileManager {
+public class TileManager
+{
     GamePanel gamePanel;
     Tile[] tile;
     int mapTileNum[][];
-
-    public TileManager(GamePanel gamePanel) {
+    int normalizedPlayerScreenX;
+    int normalizedPlayerScreenY;
+    public TileManager(GamePanel gamePanel)
+    {
         this.gamePanel = gamePanel;
+        normalizedPlayerScreenX = gamePanel.screenWidth / 2 - gamePanel.tileSize/2;
+        normalizedPlayerScreenY = gamePanel.screenHeight / 2 - gamePanel.tileSize/2;
         tile = new Tile[20]; //if ya want more kinda tile, ya can change 20 to the number ya want
         mapTileNum = new int[50][50];
         getTileImage();
+
         loadMap("/Map/map3.txt");
     }
 
-    public void getTileImage() {
-        try {
-
+    public void getTileImage()
+    {
+        try
+        {
             tile[1] = new Tile();
             tile[1].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/Tile/sand.png")));
 
             tile[0] = new Tile();
             tile[0].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/Tile/water.png")));
 
-        } catch (IOException e) {
+        }
+        catch (IOException e)
+        {
             e.printStackTrace();
         }
     }
 
-    public void loadMap(String filePath) {
-        try {
+    public void loadMap(String filePath)
+    {
+        try
+        {
             InputStream is = getClass().getResourceAsStream(filePath);
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(is));
             int col = 0;
             int row = 0;
-            while (col < 50 && row < 50) {
+            while (col < 50 && row < 50)
+            {
                 String line = bufferedReader.readLine();
-                while (col < 50) {
+                while (col < 50)
+                {
                     String numbers[] = line.split("");
                     int num = Integer.parseInt(numbers[col]);
                     mapTileNum[col][row] = num;
@@ -57,12 +70,15 @@ public class TileManager {
                 }
             }
             bufferedReader.close();
-        } catch (Exception e) {
-
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
         }
     }
 
-    public void draw(Graphics2D g2) {
+    public void draw(Graphics2D g2)
+    {
         int worldcol = 0;
         int worldrow = 0;
 
@@ -70,24 +86,22 @@ public class TileManager {
             int tileNum = mapTileNum[worldcol][worldrow];
             int worldX = worldcol * gamePanel.tileSize;
             int worldY = worldrow * gamePanel.tileSize;
-            int screenX = worldX - gamePanel.player.worldX + gamePanel.player.screenX;
-            int screenY = worldY - gamePanel.player.worldY + gamePanel.player.screenY;
+            int screenX = worldX - gamePanel.player.worldX + normalizedPlayerScreenX;
+            int screenY = worldY - gamePanel.player.worldY + normalizedPlayerScreenY;
 
-            if (worldX + gamePanel.tileSize > gamePanel.player.worldX - gamePanel.player.screenX
-                    && worldX + gamePanel.tileSize < gamePanel.player.worldX + gamePanel.player.screenX
-                    && worldY + gamePanel.tileSize > gamePanel.player.worldY - gamePanel.player.screenY
-                    && worldY - gamePanel.tileSize < gamePanel.player.worldY + gamePanel.player.screenY) {
+            if (worldX + gamePanel.tileSize > gamePanel.player.worldX - normalizedPlayerScreenX
+                    && worldX < gamePanel.player.worldX + normalizedPlayerScreenX + 48
+                    && worldY + gamePanel.tileSize > gamePanel.player.worldY - normalizedPlayerScreenY
+                    && worldY < gamePanel.player.worldY + normalizedPlayerScreenY + 48){
                 //g2.drawImage(tile[tileNum].image, screenX, screenY, 64*gp.scale, 64*gp.scale, null);
+                g2.drawImage(tile[tileNum].image, screenX, screenY, gamePanel.tileSize, gamePanel.tileSize, null);
             }
-            g2.drawImage(tile[tileNum].image, screenX, screenY, gamePanel.tileSize, gamePanel.tileSize, null);
+
             worldcol++;
             if (worldcol == 50){
                 worldcol=0;
                 worldrow++;
-
             }
         }
-
     }
-
 }
