247c925d5628becd5b03109b1ea0e8b0c349bd15
Nguyễn Bạo <robertpamvn@gmail.com>
2024-12-23 17:38:12 +0700

    Add load Tile Set procedural: - tile image - map data ( collision of tile match with tile image) Rename variable

--- src/CoreGame/MapComponent/TileManager.java
+++ src/CoreGame/MapComponent/TileManager.java
@@ -2,11 +2,11 @@ package CoreGame.MapComponent;
 
 import CoreGame.GamePanel;
 import HelpDevGameTool.ImageLoader;
+
+import javax.imageio.ImageIO;
 import java.awt.*;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.awt.image.BufferedImage;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Objects;
 
@@ -16,7 +16,9 @@ import static CoreGame.Data.Enums.Collision.NoCollision;
 public class TileManager
 {
     private static TileManager Inst;
-    public static final Tile[] tiles = new Tile[10]; // Types of tile
+    private final ArrayList<String> tileNames = new ArrayList<>();
+    private final ArrayList<String> tileCollisionStates = new ArrayList<>();
+    public static Tile[] tiles; // Types of tile
     private static final ArrayList<int[][]> tileTypeMapList = new ArrayList<>(GamePanel.maxMap);
     public static int tileTypeMap[][][] ; // [map index][col][row]
     private static final int[] maxWidth = new int[GamePanel.maxMap];
@@ -39,19 +41,124 @@ public class TileManager
         return Inst;
     }
 
+    public static void DrawTiles(final Graphics2D g2)
+    {
+        int currMapIndex = GamePanel.GetInst().currentMapIndex;
+        int playerWorldX = GamePanel.GetInst().player.worldX;
+        int playerWorldY = GamePanel.GetInst().player.worldY;
+        if(tileTypeMap[currMapIndex] == null) return;
+        for(int i = 0; i < tileTypeMap[currMapIndex].length; i++)
+        {
+            if(tileTypeMap[currMapIndex][i] == null) return;
+            for(int j = 0; j < tileTypeMap[currMapIndex][i].length; j++)
+            {
+                int typeTileIndex = tileTypeMap[currMapIndex][i][j];
+                if(typeTileIndex<0) continue;
+                int worldX = j * GamePanel.tileSize;
+                int worldY = i * GamePanel.tileSize;
+                int screenX = worldX - playerWorldX + normalizedPlayerScreenX;
+                int screenY = worldY - playerWorldY + normalizedPlayerScreenY;
+                if
+                (
+                        worldX + GamePanel.tileSize > playerWorldX - normalizedPlayerScreenX
+                        && worldX < playerWorldX + normalizedPlayerScreenX + GamePanel.tileSize
+                        && worldY + GamePanel.tileSize > playerWorldY - normalizedPlayerScreenY
+                        && worldY < playerWorldY + normalizedPlayerScreenY + GamePanel.tileSize
+                )
+                {
+                    g2.drawImage(tiles[typeTileIndex].bufferedImage, screenX, screenY, GamePanel.tileSize, GamePanel.tileSize, null);
+                }
+            }
+        }
+    }
+
+    public static int GetWidthOfCurrentMap() {return maxWidth[GamePanel.GetInst().currentMapIndex];}
+
+    public static int GetHeightOfCurrentMap(){return tileTypeMap[GamePanel.GetInst().currentMapIndex].length;}
+
+    public static int GetWidthOfMap( int currentMap)
+    {
+        if (currentMap >= tileTypeMap.length) return 0;
+        return tileTypeMap[currentMap][1].length;
+    }
+
+    public static int GetHeightOfMap( int currentMap)
+    {
+        if (currentMap >= tileTypeMap.length) return 0;
+        return tileTypeMap[currentMap].length;
+    }
+
     public void getTileImage()
     {
-            tiles[0] = new Tile();
-            tiles[0].image = ImageLoader.LoadImage("/Tile/water.png");
-            tiles[0].collision = Block;
+        //the below scripts will be removed when add Tile Set:
+        tiles[0] = new Tile();
+        tiles[0].bufferedImage = ImageLoader.LoadImage("/Tile/water.png");
+        tiles[0].collision = Block;
 
-            tiles[1] = new Tile();
-            tiles[1].image = ImageLoader.LoadImage("/Tile/sand.png");
-            tiles[1].collision = NoCollision;
+        tiles[1] = new Tile();
+        tiles[1].bufferedImage = ImageLoader.LoadImage("/Tile/sand.png");
+        tiles[1].collision = NoCollision;
 
-            tiles[2] = new Tile();
-            tiles[2].image = ImageLoader.LoadImage("/Tile/wall.png");
-            tiles[2].collision = NoCollision;
+        tiles[2] = new Tile();
+        tiles[2].bufferedImage = ImageLoader.LoadImage("/Tile/wall.png");
+        tiles[2].collision = NoCollision;
+    }
+
+    private void ExtractMapDataFromTileSet(String tileDataPath)
+    {
+        try
+        {
+            InputStream inputStream = getClass().getResourceAsStream(tileDataPath);
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            String line;
+            while ((line = bufferedReader.readLine())!=null)
+            {
+                tileNames.add(line);//this can be ignored, bcz project still doesn't use
+                tileCollisionStates.add(bufferedReader.readLine()); // <=> row++
+            }
+        }
+        catch (Exception e){ e.printStackTrace();}
+    }
+
+    private void LoadTilesFromTileSheet(String filePath, String tileDataPath, int gridSizeWidth, int tileHeight)
+    {
+        ExtractMapDataFromTileSet(tileDataPath);
+        InputStream inputStream = getClass().getResourceAsStream(filePath);
+        try
+        {
+            BufferedImage OriginalImageBf = ImageIO.read(inputStream);
+            int rows = OriginalImageBf.getHeight() / tileHeight;
+            int cols = OriginalImageBf.getWidth() / gridSizeWidth;
+            tiles = new Tile[rows*cols];
+            int indexTile = 0;
+            for (int y = 0; y < rows; y++)
+            {
+                for (int x = 0; x < cols; x++)
+                {
+                    // Crop the tile
+                    BufferedImage extractedImage = OriginalImageBf.getSubimage(x * gridSizeWidth,y * tileHeight,gridSizeWidth,tileHeight);
+                    tiles[indexTile].bufferedImage = extractedImage;
+                    indexTile++;
+                }
+            }
+            System.out.println("tile extracted from tile set successfully.");
+        } catch (IOException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void LoadTilesFromTilesFolder(String folderPath, String tileDataPath)
+    {
+        ExtractMapDataFromTileSet(tileDataPath);
+        BufferedImage[] TileArr = ImageLoader.makeFlipBook(folderPath);
+        tiles = new Tile[TileArr.length];
+        int i = 0;
+        for(BufferedImage tileImage : TileArr)
+        {
+            tiles[i].bufferedImage = tileImage;
+            tiles[i].collision = tileCollisionStates.get(i).equals("true") ? Block : NoCollision;
+            i++;
+        }
     }
 
     public void LoadMap(final String filePath,final int mapIndex)
@@ -98,50 +205,4 @@ public class TileManager
         }
     }
 
-    public static void DrawTiles(final Graphics2D g2)
-    {
-        int currMapIndex = GamePanel.GetInst().currentMapIndex;
-        int playerWorldX = GamePanel.GetInst().player.worldX;
-        int playerWorldY = GamePanel.GetInst().player.worldY;
-        if(tileTypeMap[currMapIndex] == null) return;
-        for(int i = 0; i < tileTypeMap[currMapIndex].length; i++)
-        {
-            if(tileTypeMap[currMapIndex][i] == null) return;
-            for(int j = 0; j < tileTypeMap[currMapIndex][i].length; j++)
-            {
-                int typeTileIndex = tileTypeMap[currMapIndex][i][j];
-                if(typeTileIndex<0) continue;
-                int worldX = j * GamePanel.tileSize;
-                int worldY = i * GamePanel.tileSize;
-                int screenX = worldX - playerWorldX + normalizedPlayerScreenX;
-                int screenY = worldY - playerWorldY + normalizedPlayerScreenY;
-                if
-                (
-                        worldX + GamePanel.tileSize > playerWorldX - normalizedPlayerScreenX
-                        && worldX < playerWorldX + normalizedPlayerScreenX + GamePanel.tileSize
-                        && worldY + GamePanel.tileSize > playerWorldY - normalizedPlayerScreenY
-                        && worldY < playerWorldY + normalizedPlayerScreenY + GamePanel.tileSize
-                )
-                {
-                    g2.drawImage(tiles[typeTileIndex].image, screenX, screenY, GamePanel.tileSize, GamePanel.tileSize, null);
-                }
-            }
-        }
-    }
-
-    public static int GetWidthOfCurrentMap() {return maxWidth[GamePanel.GetInst().currentMapIndex];}
-
-    public static int GetHeightOfCurrentMap(){return tileTypeMap[GamePanel.GetInst().currentMapIndex].length;}
-
-    public static int GetWidthOfMap( int currentMap)
-    {
-        if (currentMap >= tileTypeMap.length) return 0;
-        return tileTypeMap[currentMap][1].length;
-    }
-
-    public static int GetHeightOfMap( int currentMap)
-    {
-        if (currentMap >= tileTypeMap.length) return 0;
-        return tileTypeMap[currentMap].length;
-    }
 }
